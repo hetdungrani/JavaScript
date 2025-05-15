fc541fa20d1b20babccf20be813212ca6b4e3e14
Het Dungrani <142708650+hetdungrani@users.noreply.github.com>
2025-05-15 16:17:41 +0000

    comparisions

---
+++ basic/comparisions.js
@@ -0,0 +1,35 @@
+// console.log(null == 0)
+// console.log(null === 0)
+// console.log(null <= 0)
+// console.log(null >=0)
+
+// console.log(undefined == 0)
+// console.log(undefined === 0)
+// console.log(undefined <= 0)
+// console.log(undefined >=0)
+
+// premitive
+    // String  
+    // Number
+    // Boolean
+    // null
+    // undefined
+    // Symbol
+    // bigInt
+
+// refrence (non premitive)
+    // array
+    // objects
+    // functions
+
+// const id = Symbol('123')
+// const anotherid = Symbol('123')
+
+// console.log(id===anotherid)
+
+// console.log(typeof(id))
+// console.log(typeof(anotherid))
+// console.log(id)
+
+const bignumber=30976923806n
+console.log(typeof(bignumber))
